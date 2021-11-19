c43bfe8d82366737a214f8fdf320f4e3d7d8c0f3
ades101rs <94116894+ades101rs@users.noreply.github.com>
2021-11-19 11:22:32 -0300

    conexao com mongodb cloud Atlas

--- index.js
+++ index.js
@@ -7,22 +7,18 @@
 const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
-const url = "mongodb://localhost:27017";
-const dbName = "ocean_bancodados_18_11_2021";
+const url = "mongodb+srv://admin:Xq2QWgtr0DgVMN1V@cluster0.keze8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const dbName = "ocean_nuvem_19_11_2021";
 
 async function main() {
 
-    /*
+
     const client = await MongoClient.connect(url);
 
     const db = client.db(dbName);
 
     const collection = db.collection("herois");
-
-    */
-
-    const collection = undefined;
-
+  
     const app = express();
 
     // Informando para o Express considerar o corpo da requisição
